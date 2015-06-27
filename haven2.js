6ff66b25b8f9b4b59a05ab16fdf49dcd2e6f96ae
Jiexi Cao <jiexicao@gmail.com>
2015-06-27 10:51:20 -0700

    local changes

--- haven2.js
+++ haven2.js
@@ -13,6 +13,7 @@ if (Meteor.isClient) {
   Template.input.events({
     'click .sendMsg': function(e) {
        _sendMessage();
+       console.log(Meteor.user());
     },
     'keyup #msg': function(e) {
       if (e.type == "keyup" && e.which == 13) {
@@ -23,20 +24,26 @@ if (Meteor.isClient) {
 
   _sendMessage = function() {
     var el = document.getElementById("msg");
-    Messages.insert({user: Meteor.user().username, msg: el.value, ts: new Date(), room: Session.get("roomname")});
+    Messages.insert({
+        user: Meteor.user().username,
+        msg: el.value,
+        ts: new Date(),
+        room: Session.get("roomname")});
     el.value = "";
     el.focus();
   };
 
   Template.messages.helpers({
     messages: function() {
-      return Messages.find({room: Session.get("roomname")}, {sort: {ts: -1}});
+      return Messages.find({
+          room: Session.get("roomname")
+      }, {sort: {ts: -1}});
     },
   roomname: function() {
       return Session.get("roomname");
     }
   });
-  
+
   Template.message.helpers({
     timestamp: function() {
       return this.ts.toLocaleString();
@@ -48,13 +55,13 @@ if (Meteor.isClient) {
       Session.set("roomname", e.target.innerText);
     }
   });
-  
+
   Template.rooms.helpers({
     rooms: function() {
       return Rooms.find();
     }
   });
-  
+
   Template.room.helpers({
   roomstyle: function() {
       return Session.equals("roomname", this.roomname) ? "font-weight: bold" : "";
@@ -78,7 +85,7 @@ if (Meteor.isServer) {
       });
     }
   });
-  
+
   Rooms.deny({
     insert: function (userId, doc) {
       return true;
@@ -106,7 +113,7 @@ if (Meteor.isServer) {
       return (userId !== null);
     }
   });
-  
+
   Meteor.publish("rooms", function () {
     return Rooms.find();
   });
